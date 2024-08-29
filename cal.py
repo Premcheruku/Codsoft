84d49951a8a1f3ca8a92100f58dd3a5c878c2dc8
Premcheruku <harshavardhanreddycheruku@gmail.com>
2024-08-29 21:20:22 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,33 @@
+def Simplecalculator():
+    print("Simple Calculator")
+    print("Select operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+
+    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero is not allowed.")
+            return
+    else:
+        print("Invalid operation. Please choose from +, -, *, /.")
+        return
+    print(f"The result of {num1} {operation} {num2} is: {result}")
+
+
+Simplecalculator()
